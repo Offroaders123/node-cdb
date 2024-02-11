61fcc6487f3ddcb7886f207c9d4945867482e5f9
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-02-11 13:19:01 -0800

    Main File Move

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var writable = require('./src/writable-cdb'),
-    readable = require('./src/readable-cdb');
-
-module.exports = {
-    writable: writable,
-    readable: readable
-};
